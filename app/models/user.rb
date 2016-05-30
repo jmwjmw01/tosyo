70c024f0417d53f2322690eaac60d2640c71c5b8
kanitama <jmwjmw01@gmail.com>
2016-05-30 07:04:54 +0000

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  enum role: {admin: 'admin', member: 'member'}
+  
+  if user.admin?
+    can :manage, :all
+  else
+    can :update, user
+end
+end
